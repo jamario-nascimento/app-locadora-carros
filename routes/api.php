4c6274deb4a4577f9084ae250ee0bf9db81897f5
Jamário Nascimento <jamariobatista@gmail.com>
2024-04-13 13:14:15 -0300

    recuperando dados do usuario atravez do token jwt

--- routes/api.php
+++ routes/api.php
@@ -26,17 +26,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
  * e o group permite agrupar as rotas
  */
 
-Route::prefix('v1')->middleware('jwt.auth')->group( function () {
+ Route::prefix('v1')->middleware('jwt.auth')->group(function() {
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
     Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
     Route::apiResource('carro', 'App\Http\Controllers\CarroController');
     Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
     Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
-    Route::apiResource('modelo', 'App\Http\Controllers\ModeloController'); 
+    Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
 });
 
-
-
-Route::post('login', 'App\Http\Controllers\AuthController@login');
-Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('login',   'App\Http\Controllers\AuthController@login');
+Route::post('logout',  'App\Http\Controllers\AuthController@logout');
 Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-Route::post('me', 'App\Http\Controllers\AuthController@me');
