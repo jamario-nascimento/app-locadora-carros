eb5718c1186d15d452668c1be9f793cbc4beb684
Jamário Nascimento <jamariobatista@gmail.com>
2024-04-19 20:24:55 -0300

    componentizando card, input container e tablela

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,29 +2,53 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Busca de marcas</div>
+                <div class="card mb-3">
+                    <card-component titulo="Busca de marcas">
+                        <template v-slot:conteudo>
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <input-container-component titulo="ID" id="inputId" id-help="idHelp"
+                                        texto-ajuda="Opcional. Informe o ID da marca">
 
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                                <label for="inputId" class="form-label">ID</label>
-                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                                <div id="idHelp" class="form-text text-muted">Opcional. Informe o ID do registro</div>
+                                        <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
+                                            placeholder="ID">
+
+                                    </input-container-component>
+
+
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <input-container-component titulo="Nome" id="inputNome" id-help="nomeHelp"
+                                        texto-ajuda="Opcional. Informe o nome da marca">
+
+                                        <input type="text" class="form-control" id="inputNome"
+                                            aria-describedby="nomeHelp" placeholder="Nome da marca">
+
+                                    </input-container-component>
+                                </div>
                             </div>
-                            <div class="col-md-6 mb-3">
-                                <label for="inputNome" class="form-label">Nome</label>
-                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
-                                <div id="nomeHelp" class="form-text text-muted">Opcional. Informe o nome da marca</div>
-                            </div>
-                        </div>
+                        </template>
 
-                    </div>
-
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
-                    </div>
+                        <template v-slot:rodape>
+                            <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                        </template>
+                    </card-component>
                 </div>
+
+                <!-- inicio card listagem de marcas -->
+                <card-component titulo="Listagem de marcas">
+                
+                    <template v-slot:conteudo>
+                        <div class="row">
+                            <table-component></table-component>
+                        </div>
+                    </template>
+
+                    <template v-slot:rodape>
+                        <button type="buton" class="btn btn-primary btn-sm float-right">Adicionar</button>
+                    </template>
+                </card-component>
+                <!-- final listagem de marcas -->
             </div>
         </div>
     </div>
